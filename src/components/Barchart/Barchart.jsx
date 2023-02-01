8b288d1848c01843eb28de3df1bd68591f7d9a9a
Thierry-Heinz <studioklatz@gmail.com>
2023-02-01 11:49:52 +0100

    stylized BarChart

--- src/components/Barchart/Barchart.jsx
+++ src/components/Barchart/Barchart.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
 import PropTypes from "prop-types";
+import { WrapperContainer, Title, StyledResponsiveContainer } from "./style";
 
 import {
   BarChart,
@@ -9,14 +9,20 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer,
 } from "recharts";
 
-const StyledResponsiveContainer = styled(ResponsiveContainer)`
-  width: 100% !important;
-  height: 500px !important;
-  background-color: ${(props) => props.backgroundColor};
-`;
+const CustomTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${payload[0].value}kg`}</p>
+        <p className="label">{`${payload[1].value}Kcal`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 /**
  *
@@ -24,70 +30,85 @@ const StyledResponsiveContainer = styled(ResponsiveContainer)`
  */
 export default function Barchart({ data, colors }) {
   return (
-    <StyledResponsiveContainer
-      width="100%"
-      height="100%"
-      backgroundColor={colors.backgroundColor}
-    >
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20,
-          right: 20,
-          left: 20,
-          bottom: 20,
-        }}
-        barGap={"5%"}
-        barCategoryGap={"20%"}
-        barSize={8}
-      >
-        <CartesianGrid strokeDasharray="2" vertical={false} />
-        <Tooltip />
-        <Legend verticalAlign="top" align="right" margin={{ bottom: 20 }} />
-        <XAxis
-          dataKey="day"
-          tickLine={false}
-          // padding={{ left: -45, right: -45 }}
-        />
-        <YAxis
-          yAxisId={"right"}
-          tickLine={false}
-          dataKey="kilogram"
-          orientation="right"
-          axisLine={false}
-          domain={["dataMin - 10", "dataMax + 5"]}
-          margin={{ top: 50, right: 20 }}
-        />
-        <YAxis
-          yAxisId={"left"}
-          axisLine={false}
-          tickLine={false}
-          domain={["dataMin", 450]}
-          dataKey="calories"
-          orientation="left"
-          hide={true}
-        />
-        <Bar
-          yAxisId="right"
-          dataKey="kilogram"
-          name="Kilogrammes (kg)"
-          legendType="circle"
-          fill={colors.dataKey1Color}
-          background={{ fill: "#eee" }}
-          radius={[3.5, 3.5, 0, 0]}
-        />
-        <Bar
-          yAxisId="left"
-          radius={[3.5, 3.5, 0, 0]}
-          dataKey="calories"
-          legendType="circle"
-          name="Calories brûlées (kCal)"
-          fill={colors.dataKey2Color}
-        />
-      </BarChart>
-    </StyledResponsiveContainer>
+    <WrapperContainer>
+      <Title>Activité quotidienne</Title>
+
+      <StyledResponsiveContainer backgroundColor={colors.backgroundColor}>
+        <BarChart
+          width={1200}
+          height={500}
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            left: 20,
+            bottom: 20,
+          }}
+          barGap={"5%"}
+          barCategoryGap={"20%"}
+          barSize={8}
+        >
+          <CartesianGrid
+            strokeDasharray="2"
+            vertical={false}
+            padding={{ top: 10, bottom: 10 }}
+            margin={{ top: 50, right: 20 }}
+          />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend
+            verticalAlign="top"
+            align="right"
+            margin={{ top: 10, bottom: 20 }}
+            padding={{ top: 10, bottom: 10 }}
+            height={60}
+          />
+          <XAxis
+            dataKey="day"
+            tickLine={false}
+            padding={{ left: 10, right: 10 }}
+            axisLine={false}
+          />
+          <YAxis
+            yAxisId={"right"}
+            tickLine={false}
+            dataKey="kilogram"
+            orientation="right"
+            axisLine={false}
+            domain={["dataMin - 10", "dataMax + 5"]}
+            padding={{ top: 10, bottom: 10 }}
+            margin={{ top: 50, right: 20 }}
+          />
+          <YAxis
+            yAxisId={"left"}
+            axisLine={false}
+            tickLine={false}
+            domain={["dataMin", 450]}
+            dataKey="calories"
+            orientation="left"
+            padding={{ top: 10, bottom: 10 }}
+            hide={true}
+          />
+          <Bar
+            yAxisId="right"
+            dataKey="kilogram"
+            name="Kilogrammes (kg)"
+            legendType="circle"
+            fill={colors.dataKey1Color}
+            background={{ fill: "#eee" }}
+            radius={[3.5, 3.5, 0, 0]}
+            margin={{ top: 10, left: 10, right: 10, bottom: 10 }}
+          />
+          <Bar
+            yAxisId="left"
+            radius={[3.5, 3.5, 0, 0]}
+            dataKey="calories"
+            legendType="circle"
+            name="Calories brûlées (kCal)"
+            fill={colors.dataKey2Color}
+          />
+        </BarChart>
+      </StyledResponsiveContainer>
+    </WrapperContainer>
   );
 }
 
---
+++ src/components/Barchart/style.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import { ResponsiveContainer } from "recharts";
+import colors from "../../utils/colors";
+
+export const WrapperContainer = styled.div`
+  position: relative;
+`;
+
+export const Title = styled.h3`
+  position: absolute;
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  font-size: 15px;
+  color: ${colors.darkgrey2};
+  margin-left: 32px;
+  line-height: 24px;
+  margin-top: 24px;
+`;
+
+export const StyledResponsiveContainer = styled(ResponsiveContainer)`
+  width: 1200px !important;
+  height: 500px !important;
+  border-radius: 5px;
+  background-color: ${(props) => props.backgroundColor};
+  .recharts-default-legend {
+    display: flex;
+    justify-content: flex-end;
+    gap: 32px;
+
+    .recharts-legend-item-text{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 500;
+        font-size: 14px;
+        color: ${colors.mediumgrey} !important;
+        margin-left: 12px;
+    }
+  }
+  .recharts-cartesian-grid-horizontal {
+    line:nth-last-child(2),
+    line:last-child {
+      display: none;
+    }
+  }
+  .recharts-bar-background-rectangle {
+    display: none;
+  }
+  .custom-tooltip {
+    background-color: ${colors.primary};
+    border: none;
+    padding:5px 10px;
+    display:flex;
+    flex-direction: column;
+    gap: 7px;
+
+    p.label {
+      font-family: 'Roboto', sans-serif;
+      font-weight: 500;
+        color: ${colors.white};
+        font-size: 12px;
+      }
+    }
+  }
+`;
